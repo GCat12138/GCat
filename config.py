72ab13786d11f963dd27f3546524588f28ecee50
= <hjxwho@gmail.com>
2014-08-06 16:18:06 +0800

    modify manage.py - import environment from .env

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 baseDir = os.path.abspath(os.path.dirname(__file__))
 
+
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
--- manage.py
+++ manage.py
@@ -1,5 +1,13 @@
 #!./env/bin/python2.7
 import os
+
+if os.path.exists('.env'):
+    print('Importing environment from .env...')
+    for line in open('.env'):
+            var = line.strip().split('=')
+            if len(var) == 2:
+                os.environ[var[0]] = var[1]
+
 from app import create_app, db
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
